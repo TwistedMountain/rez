6439747c828d8fbfbda0c8e2287353436bf760f7
Arun T <arunthomas2213@gmail.com>
2026-06-10 10:44:45 -0700

    Update rezconfig.py
    Signed-off-by: Arun T <arunthomas2213@gmail.com>

--- src/rez/rezconfig.py
+++ src/rez/rezconfig.py
@@ -57,10 +57,10 @@ import sys
 ###############################################################################
 
 ### Do not move or delete this comment (__DOC_START__)
-if 'win' in sys.platform:
-   ENVL_CUSTOM_PATH = "~/AppData/Local/TMA/ENVlaunch"
-elif 'darwin' in sys.platform:
+if sys.platform == 'darwin':
    ENVL_CUSTOM_PATH = "~/Library/Application Support/TMA/ENVlaunch"
+elif 'win' in sys.platform:
+   ENVL_CUSTOM_PATH = "~/AppData/Local/TMA/ENVlaunch"
 else:
    ENVL_CUSTOM_PATH = "~/TMA/ENVlaunch"
 
